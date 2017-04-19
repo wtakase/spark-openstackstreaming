14a4774444a5fd9e74a2d46fe03ab44d8b95029b
Wataru Takase <wataru.takase@kek.jp>
2017-04-19 11:08:53 +0900

    Use KafkaUtils.createDirectStream

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ $ mvn clean package
 ```
 
 ## Run
-Run the following command with Kafka server (i.e. 192.168.1.250) and topic_id (i.e. openstack):
+Run the following command with Kafka server (i.e. 192.168.1.250:9092) and topic_id (i.e. openstack) and Elasticsearch node (i.e. 192.168.1.250:9200):
 ```
-$ spark-submit --master local[*] --class jp.kek.spark.openstackstreaming.OpenStackStreaming target/openstackstreaming-0.0.1-jar-with-dependencies.jar 192.168.1.250 openstack
+$ spark-submit --master local[*] --class jp.kek.spark.openstackstreaming.OpenStackStreaming target/openstackstreaming-0.0.1-jar-with-dependencies.jar 192.168.1.250:9092 openstack 192.168.1.250:9200
 ```
--- src/main/scala/jp/kek/spark/openstackstreaming/OpenStackStreaming.scala
+++ src/main/scala/jp/kek/spark/openstackstreaming/OpenStackStreaming.scala
@@ -4,24 +4,49 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.Milliseconds
 import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.dstream.InputDStream
 import org.apache.spark.storage.StorageLevel
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
-import org.apache.spark.streaming.kafka._
+import org.elasticsearch.spark.rdd.EsSpark
+import kafka.serializer.StringDecoder
+import scala.util.parsing.json.JSON
 
 object OpenStackStreaming {
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]) {
     Logger.getRootLogger.setLevel(Level.WARN)
 
-    val sc = new SparkContext(new SparkConf().setAppName("OpenStack Streaming"))
-    val ssc = new StreamingContext(sc, Milliseconds(5000))
-    ssc.checkpoint("/tmp/checkpoint")
-    val kafkaStream = KafkaUtils.createStream(ssc,
-                                              args(0),
-                                              "default",
-                                              Map(args(1) -> 1))
-    kafkaStream.print()
+    val brokerList = args(0)
+    val topicId = args(1)
+    val esNodes = args(2)
+    val timestamp: Long = System.currentTimeMillis / 1000
+    val checkpoint = "/tmp/checkpoint" + "/" + topicId + "/" + timestamp.toString
+
+    val ssc = StreamingContext.getOrCreate(checkpoint,
+                () => createStreamingContext(brokerList, topicId, esNodes, checkpoint))
+
     ssc.start()
     ssc.awaitTermination()
+  }
+
+  def createStreamingContext(
+          brokerList: String, topicId: String, esNodes: String,
+          checkpoint: String): StreamingContext = {
+    val conf = new SparkConf().setAppName("OpenStack Logs Streaming").set("es.nodes", esNodes)
+    val sc = new SparkContext(conf)
+    val ssc = new StreamingContext(sc, Milliseconds(5000))
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> brokerList)
+    val kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+                        ssc, kafkaParams, Set(topicId))
+
+    run(kafkaStream)
+    ssc.checkpoint(checkpoint)
+    ssc
+  }
+
+  def run(stream: InputDStream[(String, String)], windowLength: Int = 30, slideInterval: Int = 5) {
+    stream.foreachRDD(rdd => rdd.collect().foreach(println))
+    stream.map(_._2).map(JSON.parseFull(_)).foreachRDD(rdd => EsSpark.saveToEs(rdd, "openstack/logs"))
   }
 }
